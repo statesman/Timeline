cad7dba48df0d63683fe6f1ec6d7ee62eca1e8f0
Andrew Chavez <adchavez@gmail.com>
2014-12-09 16:59:15 -0600

    Create a Timeline class, allowing multiple per page

--- js/script.js
+++ js/script.js
@@ -2,19 +2,205 @@
 var timelineConfig = {
 	key: '0AvZh5Gsgu1WvdFo5VjB3dnBhSlVzN2hxejdsNmpHTFE',
 	sheetName: 'Posts' // change to name of spreadsheet 'sheet' that contains the data
+};
+
+var Timeline = function(el, data, tabletop) {
+	this.$el = $(el);
+	this.$el.addClass('vertical-timeline');
+
+	var controlTemplate = Handlebars.compile($('#controls-template').html());
+	this.$el.append(controlTemplate());
+
+	this.$el.append('<div class="timeline-items"></div>');
+	this.$timeline = this.$el.find('.timeline-items');
+
+	var direction = 'newest';
+
+	// load the Handlebar templates into memory
+	var postTemplate  = Handlebars.compile($('#post-template').html());
+	var yearMarkerTemplate  = Handlebars.compile($('#year-marker-template').html());
+
+	var self = this;
+
+	var years = [];
+
+	$.each(tabletop.sheets(timelineConfig.sheetName).all(), function(i, val){
+		// save the years so we can create year markers
+		var year = new Date(val.timestamp).getFullYear();
+		if (years.indexOf(year) < 0) {
+			years[years.length] = year;
+		}
+
+		// combine data & templqate
+		var html = postTemplate(val);
+		self.$timeline.append(html);
+	});
+
+	// add a year marker for each year that has a post
+	$.each(years, function(i, val){
+		var timestamp;
+		if (direction == 'newest') {
+			timestamp = self._getTimestamp(val, false);
+		}
+		else {
+			timestamp = self._getTimestamp(val, true);
+		}
+		var context = {year: val, timestamp: timestamp};
+		var html = yearMarkerTemplate(context);
+		self.$timeline.append(html);
+	});
+
+	var lineTemplate = Handlebars.compile($('#line-template').html());
+	this.$el.append(lineTemplate());
+
+	this.$timeline.imagesLoaded(function(){
+		this.isotope({
+			itemSelector : '.item',
+			transformsEnabled: true,
+			layoutMode: 'spineAlign',
+			spineAlign:{
+				gutterWidth: 56
+			},
+			getSortData: {
+				timestamp: function($elem){
+					return parseFloat($elem.find('.timestamp').text());
+				}
+			},
+			sortBy: 'timestamp',
+			sortAscending: false,
+			itemPositionDataEnabled: true
+							});
+	});
+
+	// load scripts after all the html has been set
+	$.getScript('//static.ak.fbcdn.net/connect.php/js/FB.Share');
+	$.getScript('//platform.twitter.com/widgets.js');
+
+	// add open/close buttons to each post
+	this.$el.each(function(){
+		$(this).find('.inner').append('<a href="#" class="open-close"></a>');
+	});
+
+	this.$el.find('.item a.open-close').click(function(e){
+		$(this).siblings('.body').slideToggle(function(){
+			self.$timeline.isotope('reLayout');
+							});
+		$(this).parents('.post').toggleClass('closed');
+		self.$el.find('.expand-collapse-buttons a').removeClass('active');
+		e.preventDefault();
+	});
+
+	this.$el.find('.post .share').hover(
+		function(){
+			$(this).find('.share-trigger').addClass('over');
+			$(this).find('.share-popup').show();
+		},
+		function(){
+			$(this).find('.share-trigger').removeClass('over');
+			$(this).find('.share-popup').hide();
+		}
+	);
+
+	this._setupControls();
+
+	this._adjustLine();
+	this.$el.resize($.proxy(this._adjustLine, this));
+};
+
+Timeline.prototype._setupControls = function() {
+	var self = this;
+	var controls = this.$el.find('.controls');
+	controls.find('a.expand-all').click(function(e){
+		self.$el.find('.post .body').slideDown(function(){
+			self.$timeline.isotope('reLayout');
+							});
+		self.$el.find('.post').removeClass('closed');
+		self.$el.find('.expand-collapse-buttons a').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+	controls.find('a.collapse-all').click(function(e){
+		self.$el.find('.post .body').slideUp(function(){
+			self.$timeline.isotope('reLayout');
+		});
+		self.$el.find('.post').addClass('closed');
+		self.$el.find('.expand-collapse-buttons a').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+	controls.find('.sort-buttons a').click(function(e){
+							var $this = $(this);
+							// don't proceed if already selected
+							if ($this.hasClass('active')) {
+								return false;
+							}
+
+		controls.find('.sort-buttons a').removeClass('active');
+							$this.addClass('active');
+							var $yearMarkers = $('.year-markers');
+							if ($this.hasClass('sort-newest')){
+			self._updateYearMarkers(false);
+			self.$timeline.isotope('reloadItems').isotope({sortAscending: false});
+							}
+							else{
+			self._updateYearMarkers(true);
+			self.$timeline.isotope('reloadItems').isotope({sortAscending: true});
+							}
+							e.preventDefault();
+						});
+};
+
+Timeline.prototype._adjustLine = function() {
+	var $lastItem = this.$el.find('.item.last');
+	var itemPosition = $lastItem.data('isotope-item-position');
+	var dateHeight = $lastItem.find('.date').height();
+	var dateOffset = $lastItem.find('.date').position();
+	var innerMargin = parseInt($lastItem.find('.inner').css('marginTop'));
+	var lineHeight = itemPosition.y + innerMargin + dateOffset.top + (dateHeight / 2);
+	this.$el.find('.line').height(lineHeight);
+};
+
+/**
+* Get the timestamp (milliseconds) given the year.
+* If beginning is true, timestamp is 1/1/year 12:00:00:000
+* If beginning is false, timestamp is 12/31/year 23:59:59:999
+*/
+Timeline.prototype._getTimestamp = function(year, beginning) {
+	if (beginning) {
+		return Date.parse('January 1, ' + year);
+	}
+	else {
+		return Date.parse('December 31, ' + year) + 86400000 - 1 ; // plus 1 day, minus 1 millisecond
+	}
+};
+
+/*
+* Set the timestamp of the year markers to either the beginning or end of
+* the year
+*/
+Timeline.prototype._updateYearMarkers = function(beginning) {
+	var self = this;
+	self.$el.find('.year-marker').each(function(){
+		var $this = $(this);
+		var year = parseInt($this.find('.year').text());
+		var timestamp = self._getTimestamp(year, beginning);
+		$this.find('.timestamp').text(timestamp);
+	});
 }
 
 
 $(function() {
 
-	var direction = 'newest';
-
 	/**
-	* get data via Tabletop
-	*/
+	 * get data via Tabletop
+	 */
 	Tabletop.init({
 		key: timelineConfig.key,
-		callback: setupTimeline,
+		callback: function(data, tabletop) {
+			new Timeline('#timeline', data, tabletop);
+		},
 		wanted: [timelineConfig.sheetName],
 		postProcess: function(el){
 			el['timestamp'] = Date.parse(el['date']);
@@ -22,174 +208,8 @@ $(function() {
 			el['read_more_url'] = el['readmoreurl'];
 			el['photo_url'] = el['photourl'];
 		}
-	});
-
-	/**
-	* Get the timestamp (milliseconds) given the year.
-	* If beginning is true, timestamp is 1/1/year 12:00:00:000
-	* If beginning is false, timestamp is 12/31/year 23:59:59:999
-	*/
-	function getTimestamp(year, beginning){
-		if (beginning)
-			return Date.parse('January 1, ' + year);
-			else
-				return Date.parse('December 31, ' + year) + 86400000 - 1 ; // plus 1 day, minus 1 millisecond
-			}
-
-			/**
-			* Load the data into Isotope
-			*/
-			function setupTimeline(data, tabletop){
-				// load the Handlebar templates into memory
-				var source = $('#post-template').html();
-				var postTemplate  = Handlebars.compile(source);
-				source = $('#year-marker-template').html();
-				var yearMarkerTemplate  = Handlebars.compile(source);
-
-				var years = [];
-				$.each(tabletop.sheets(timelineConfig.sheetName).all(), function(i, val){
-					// save the years so we can create year markers
-					var year = new Date(val.timestamp).getFullYear();
-					if (years.indexOf(year) < 0)
-						years[years.length] = year;
-
-						// combine data & templqate
-						var html = postTemplate(val);
-						$('#timeline').append(html);
 					});
 
-					// add a year marker for each year that has a post
-					$.each(years, function(i, val){
-						var timestamp;
-						if (direction == 'newest')
-							timestamp = getTimestamp(val, false);
-							else
-								timestamp = getTimestamp(val, true);
-								var context = {year: val, timestamp: timestamp};
-								var html = yearMarkerTemplate(context);
-								$('#timeline').append(html);
-							});
-
-							$('#timeline').imagesLoaded(function(){
-								$('#timeline').isotope({
-									itemSelector : '.item',
-									transformsEnabled: true,
-									layoutMode: 'spineAlign',
-									spineAlign:{
-										gutterWidth: 56
-									},
-									getSortData: {
-										timestamp: function($elem){
-											return parseFloat($elem.find('.timestamp').text());
-										}
-									},
-									sortBy: 'timestamp',
-									sortAscending: false,
-									itemPositionDataEnabled: true
-								});
-							});
-
-							// load scripts after all the html has been set
-							$.getScript('//static.ak.fbcdn.net/connect.php/js/FB.Share');
-							$.getScript('//platform.twitter.com/widgets.js');
-
-							// add open/close buttons to each post
-							$('#timeline .item.post').each(function(){
-								$(this).find('.inner').append('<a href="#" class="open-close"></a>');
-							});
-
-							$('#timeline .item a.open-close').click(function(e){
-								$(this).siblings('.body').slideToggle(function(){
-									$('#timeline').isotope('reLayout');
-								});
-								$(this).parents('.post').toggleClass('closed');
-								$('#expand-collapse-buttons a').removeClass('active');
-								e.preventDefault();
-							});
-
-							$('#timeline .post .share').hover(
-								function(){
-									$(this).find('.share-trigger').addClass('over');
-									$(this).find('.share-popup').show();
-								},
-								function(){
-									$(this).find('.share-trigger').removeClass('over');
-									$(this).find('.share-popup').hide();
-								}
-							);
-
-							$('#buttons a.expand-all').click(function(e){
-								$('.post .body').slideDown(function(){
-									$('#timeline').isotope('reLayout');
-								});
-								$('.post').removeClass('closed');
-								$('#expand-collapse-buttons a').removeClass('active');
-								$(this).addClass('active');
-								e.preventDefault();
-							});
-
-							$('#buttons a.collapse-all').click(function(e){
-								$('.post .body').slideUp(function(){
-									$('#timeline').isotope('reLayout');
-								});
-								$('.post').addClass('closed');
-								$('#expand-collapse-buttons a').removeClass('active');
-								$(this).addClass('active');
-								e.preventDefault();
-							});
-
-						}
-
-						/*
-						* Set the timestamp of the year markers to either the beginning or end of
-						* the year
-						*/
-						function updateYearMarkers(beginning){
-							$('.year-marker').each(function(){
-								var $this = $(this);
-								var year = parseInt($this.find('.year').text());
-								var timestamp = getTimestamp(year, beginning);
-								$this.find('.timestamp').text(timestamp);
-							});
-						}
-
-						/*
-						* Keep the actual line from extending beyond the last item's date tab
-						*/
-						function adjustLine(){
-							var $lastItem = $('.item.last');
-							var itemPosition = $lastItem.data('isotope-item-position');
-							var dateHeight = $lastItem.find('.date').height();
-							var dateOffset = $lastItem.find('.date').position();
-							var innerMargin = parseInt($lastItem.find('.inner').css('marginTop'));
-							var lineHeight = itemPosition.y + innerMargin + dateOffset.top + (dateHeight / 2);
-							$('#line').height(lineHeight);
-						}
-
-						$('#sort-buttons a').click(function(e){
-							var $this = $(this);
-							// don't proceed if already selected
-							if ($this.hasClass('active')) {
-								return false;
-							}
-
-							$('#sort-buttons a').removeClass('active');
-							$this.addClass('active');
-							var $yearMarkers = $('.year-markers');
-							if ($this.hasClass('sort-newest')){
-								updateYearMarkers(false);
-								$('#timeline').isotope('reloadItems').isotope({sortAscending: false});
-							}
-							else{
-								updateYearMarkers(true);
-								$('#timeline').isotope('reloadItems').isotope({sortAscending: true});
-							}
-							e.preventDefault();
-						});
-
-						$('#timeline').resize(function(){ // uses "jQuery resize event" plugin
-						adjustLine();
-					});
 				});
 
 
